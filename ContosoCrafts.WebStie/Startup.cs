065bd84384f175ab2a85fc6b4c24d90bbd04284f
Canberk Sahin <canberksahin20@hotmail.com>
2020-09-26 01:47:18 +0300

    Blazor page

--- ContosoCrafts.WebStie/Startup.cs
+++ ContosoCrafts.WebStie/Startup.cs
@@ -29,6 +29,7 @@ namespace ContosoCrafts.WebStie
         {
             services.AddRazorPages();
             services.AddControllers();
+            services.AddServerSideBlazor();
             services.AddTransient<JsonFileProductService>();
         }
 
@@ -57,6 +58,7 @@ namespace ContosoCrafts.WebStie
             {
                 endpoints.MapRazorPages();
                 endpoints.MapControllers();
+                endpoints.MapBlazorHub();
                 //endpoints.MapGet("/products", (context) =>
                 // {
                 //     var products = app.ApplicationServices.GetService<JsonFileProductService>().GetProducts();
